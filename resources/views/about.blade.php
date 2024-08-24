20dc15c45ae4f1a8297a300b0d9a8dd30e636fcb
ginandaqwe123 <ucing97@gmail.com>
2024-08-24 15:40:16 +0700

    Laravel Pertama

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h1>Selamat Datang di Halaman About</h1>
+    <p>Nama: {{ $name }}</p>
+    <br>
+    <img src="img/dota.jpg" alt="" width="500">
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h1>Halaman Contact</h1>
+    {{-- <ul>
+        <li>
+            <a href="https://www.instagram.com/ginan.nanzz/">Instagram</a>
+        </li>
+        <li>
+            <a href="https://www.youtube.com/@ntudin">Youtube</a>
+        </li>
+    </ul> --}}
+</x-layout>
